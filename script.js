460f20e3c1a770bfbbe7698e57f40ed6a57e75c2
Marionwanyama23 <marionwanyama23@gmail.com>
2025-03-19 16:04:41 +0300

    completed the project

--- script.js
+++ script.js
@@ -3,15 +3,37 @@ const listContainer = document.getElementById('list-container')
 
 function addtask() {
     if(inputBox.value === '') {
-        inputBox.classList.add('highlight');
-    setTimeout(() => {
-        inputBox.classList.remove('highlight');
-    }, 2000);
+        alert('You must add a task!')
     } else {
         let li = document.createElement('li');
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
     }
 
     inputBox.value = '';
+    saveData();
 }
+
+listContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+
+    } else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false)
+
+//save the list in local storage
+let saveData = () =>{
+    localStorage.setItem('data', listContainer.innerHTML)
+}
+
+//show the list items on the browser after refreshing
+let showTask = () => listContainer.innerHTML = localStorage.getItem('data');
+
+showTask();
